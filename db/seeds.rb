5ec93cf31c01750e64c51bca3629568da0f524c0
richard trapanese <richardtrapanese@gmail.com>
2018-09-24 22:11:28 -0400

    created seed file tested and applied to local host passed but had to remove FK constraints in postgresql pushing to heroku then test that

---
+++ db/seeds.rb
@@ -0,0 +1,435 @@
+
+User.create(
+    username: 'orlando',
+    password: 'password',
+    first_name: 'orlando',
+    last_name: 'caraballo',
+    birthday: '01011960',
+    email: 'orlando.caraballo@nycda.com',
+    code_lang: 'Ruby'
+  )
+  
+  User.create(
+    username: 'diana',
+    password: 'password',
+    first_name: 'orlando',
+    last_name: 'caraballo',
+    birthday: '01011960',
+    email: 'orlando.caraballo@nycda.com',
+    code_lang: 'Ruby'
+  )
+  
+  User.create(
+    username: 'janice',
+    password: 'password',
+    first_name: 'orlando',
+    last_name: 'caraballo', 
+    birthday: '01011960',
+    email: 'orlando.caraballo@nycda.com',
+    code_lang: 'Ruby'
+  )
+  User.create(
+    username: 'madCoder',
+    password: 'password',
+    first_name: 'richard',
+    last_name: 'trapanese',
+    birthday: '01011960',
+    email: 'richardtrapanese@gmail.com',
+    code_lang: 'Ruby'
+  )
+  Post.create(
+      title: 'hello',
+      content: 'nice place to talk code',
+      users_id: 1
+  )
+  Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 1
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 1
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 1
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 1
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 1
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 1
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 1
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 1
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 1
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 1
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 1
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 1
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 1
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 1
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 1
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 1
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 1
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 1
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 1
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 2
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 2
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 2
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 2
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 2
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 2
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 2
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 2
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 2
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 2
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 2
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 2
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 2
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 2
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 2
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 2
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 2
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 2
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 2
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 2
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 2
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 3
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 3
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 3
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 3
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 3
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 3
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 3
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 3
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 3
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 3
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 3
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 3
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 3
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 3
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 3
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 3
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 3
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 3
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 3
+)
+
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 4
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 4
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 4
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 4
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 4
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 4
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 4
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 4
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 4
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 4
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 4
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 4
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 4
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 4
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 4
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 4
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 4
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 4
+)
+Post.create(
+    title: 'hello',
+    content: 'nice place to talk code',
+    users_id: 4
+)
